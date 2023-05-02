e955fedde1c6bbd830bcb875e85b08c9bedc0a82
Helen Byelyayeva <91469540+helenbyelyayeva@users.noreply.github.com>
2023-05-03 02:56:19 +0300

    fixed-statistic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,6 +18,7 @@ export const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
       />
+      <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
--- src/components/Statistic-task/Statistics.jsx
+++ src/components/Statistic-task/Statistics.jsx
@@ -1,11 +1,11 @@
 import propTypes from 'prop-types';
 import css from "./Statistics.module.css";
 
-export const Statistics = ({ stats }) => {
+export const Statistics = ({ title, stats }) => {
     return (
         <section className={css.statistics}>
             <div className={css.upload}>
-                <h2 className={css.title}>Upload stats</h2>
+                {title && <h2 className={css.title}>{title}</h2>}
                 <ul className={css.stat_list}>
                     {stats.map(({ id, label, percentage }) => (
                         <li className={css.item} key={id} >
@@ -20,12 +20,13 @@ export const Statistics = ({ stats }) => {
 }
 
 Statistics.propTypes = {
+    title: propTypes.string,
     stats: propTypes.arrayOf(
-        propTypes.shape({
-            id: propTypes.string,
+        propTypes.exact({
+            id: propTypes.string.isRequired,
             label: propTypes.string.isRequired,
             percentage: propTypes.number.isRequired,
         })
     ),
-}
+};
 
